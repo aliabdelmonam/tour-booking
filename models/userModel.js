d8f3a0ec8cb806a5d73f4cebb1f216ebca6dd369
al-qa-qa <alqaqa.fighter@gmail.com>
2025-04-26 11:43:05 +0300

    modifications to user model, and setting up routers

--- models/userModel.js
+++ models/userModel.js
@@ -2,57 +2,60 @@ import mongoose from "mongoose";
 import { validateEmail, validatePassword } from "../utils/validate.js";
 import { hashPasswordHook } from "../middlewares/mogooseHooks.js";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please tell use your name"],
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, "Please provide email address"],
-    lowercase: true,
-    validate: {
-      // This only works on SAVING and CREATING and not in updating.
-      validator: (val) => validateEmail(val),
-      message: "Please provide a valid email address",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please tell use your name"],
     },
-  },
-  role: {
-    type: String,
-    enum: ["user", "guide", "admin"],
-    default: "user",
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-    minLength: 8,
-    validate: {
-      // This only works on SAVING and CREATING and not in updating.
-      validator: (val) => validatePassword(val),
-      message:
-        "Passwords Should be at least 8 characters long, with at least one letter and one digit",
-    },
-
-    select: false,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, "Please confirm your password"],
-    validate: {
-      // This only works on SAVING and CREATING and not in updating.
-      validator: function (e) {
-        return e === this.password;
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide email address"],
+      lowercase: true,
+      validate: {
+        // This only works on SAVING and CREATING and not in updating.
+        validator: (val) => validateEmail(val),
+        message: "Please provide a valid email address",
       },
-      message: "Passwords are not the same",
+    },
+    role: {
+      type: String,
+      enum: ["user", "guide", "admin"],
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minLength: 8,
+      validate: {
+        // This only works on SAVING and CREATING and not in updating.
+        validator: (val) => validatePassword(val),
+        message:
+          "Passwords Should be at least 8 characters long, with at least one letter and one digit",
+      },
+
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        // This only works on SAVING and CREATING and not in updating.
+        validator: function (e) {
+          return e === this.password;
+        },
+        message: "Passwords are not the same",
+      },
+    },
+    active: {
+      type: Boolean,
+      default: true,
+      select: false,
     },
   },
-  active: {
-    type: Boolean,
-    default: true,
-    select: false,
-  },
-});
+  { timestamps: true },
+);
 
 userSchema.pre("save", hashPasswordHook);
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import {
+  createUser,
+  getAllUsers,
+  getUser,
+  login,
+  signUp,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.route("/").post(createUser);
+userRouter.route("/login").post(login);
+userRouter.route("/signup").post(signUp);
+userRouter.route("/").get(getAllUsers).post(createUser);
+userRouter.route("/:id").get(getUser);
 
 export default userRouter;
