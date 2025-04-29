bbf3d44245230fc9df172bede5019ad9d9e395e1
Muhammad Khaled <muhammadhaggagg@gmail.com>
2025-04-29 07:43:44 +0300

    Add swagger code of the discount module

--- routes/discountRouter.js
+++ routes/discountRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
 import * as discountController from "../controllers/discountController.js";
-import { tokenAuth } from "../middlewares/auth.js";
+import { tokenAuth,  restrictTo} from "../middlewares/auth.js";
 
 const discountRouter = express.Router();
 
 
 discountRouter.use(tokenAuth);
-discountRouter.route("/").post(discountController.createDiscount);
 discountRouter.route("/validate").post(discountController.validateDiscount);
 discountRouter.route("/apply").post(discountController.applyDiscount);
+discountRouter.use(restrictTo("admin"));
+discountRouter.route("/").post(discountController.createDiscount);
 discountRouter.route('/').get(discountController.getAllDiscounts);
 discountRouter.route('/:id').get(discountController.getDiscountById).patch(discountController.updateDiscount).delete(discountController.deleteDiscount);
 
