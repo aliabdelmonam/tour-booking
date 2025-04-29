cf27792e4b22e64d8901ab44180dee5ab5384847
al-qa-qa <alqaqa.fighter@gmail.com>
2025-04-29 09:32:06 +0300

    fix-reviews missed when resolving conflicts

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -4,15 +4,12 @@ import { restrictTo, tokenAuth } from "../middlewares/auth.js";
 
 const reviewRouter = express.Router();
 
-
 // Make <auth.tokenAuth> a Middleware for all routes down below
 reviewRouter.use(tokenAuth);
-reviewRouter
-  .route("/create_review")
-  .post( reviewController.createReview);
+reviewRouter.route("/create_review").post(reviewController.createReview);
 reviewRouter
   .route("/get_all_reviews")
-  .get( reviewController.getAllReviews);
+  .get(restrictTo("admin"), reviewController.getAllReviews);
 
 reviewRouter
   .route("/get_Review_ById/:review_id")
